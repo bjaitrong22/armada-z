7e17d54c86c88d56e09436ffbf379be6787f7410
Bai Jaitrong <bjaitrong@comcast.net>
2023-05-09 00:32:49 -0600

    BUGFIX: Fix background scrolling and add player horizontal movement

--- src/js/game.js
+++ src/js/game.js
@@ -1,6 +1,7 @@
 /** @type { HTMLCanvasElement} */
 import { Background } from "./background.js";
 import { Player } from "./player.js";
+import { InputHandler } from "./input.js";
 
 export class Game {
   constructor( width, height) {
@@ -8,13 +9,19 @@ export class Game {
     this.height = height;
     this.bottomMargin = 200;
     this.speed = 0;
-    this.maxSpeed = 2;
+    this.maxSpeed = .3;
     this.background = new Background(this);
     this.player = new Player(this);
+    this.input = new InputHandler(this);
+    this.debug = false;
+    this.time = 0;
+    this.player.currentState =  this.player.states[0];
+    this.player.currentState.enter();
   }
   update(deltaTime) {
     this.time += deltaTime;
     this.background.update();
+    this.player.update(this.input.keys, deltaTime);
   }
   draw(context) {
     this.background.draw(context);
--- src/js/player.js
+++ src/js/player.js
@@ -1,16 +1,52 @@
+import { Floating, Moving } from "./playerStates";
+
 export class Player {
   constructor(game){
     this.game = game;
     this.width = 150;
     this.height = 54;
-    this.x = 200;
+    this.x = 0;
     this.y = this.game.height - this.height - this.game.bottomMargin;
     this.image = document.getElementById('player_ship');
+    this.frameX = 0;
+    this.frameY = 0;
+    this.maxFrame;
+    this.fps = 20;
+    this.frameInterval = 1000/this.fps;
+    this.frameTime = 0;
+    this.dx = 0;
+    this.dy = 0;
+    this.maxDx = 5;
+    this.maxDy = 0;
+    this.states = [new Floating(this.game), new Moving(this.game)];
+    this.currentState = null;
+    
   }
-  update(){
+  update(input, deltaTime){
+    this.currentState.handleInput(input);
+    //horizontal movement
+    this.x += this.dx;
+    if (input.includes('ArrowRight')) this.dx = this.maxDx;
+    else if (input.includes('ArrowLeft')) this.dx = -this.maxDx;
+    else this.speed = 0;
+
+    //sprite animation
+    if (this.frameTimer > this.frameInterval) {
+      this.frameTimer = 0;
+      if (this.frameX < this.maxFrame) this.frameX++;
+      else this.frameX = 0;
+    } else {
+      this.frameTimer += deltaTime;
+    }
+  }
+  draw(context) {
+    if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
+    context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height );
 
   }
-  draw(context){
-    context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+  setState(state, dx){
+    this.currentState = this.states[state];
+    this.game.speed = this.game.maxSpeed * dx;
+    this.currentState.enter();
   }
 }
