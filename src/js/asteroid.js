aa65720a3bf616d94444c20f62689519ab6354a3
Bai Jaitrong <bjaitrong@comcast.net>
2023-05-12 20:02:12 -0600

    adjust asteroid horizontal speed

--- src/js/asteroid.js
+++ src/js/asteroid.js
@@ -7,7 +7,7 @@ export class Asteroid {
     this.image = document.getElementById('asteroid');
     this.width = 150;
     this.height = 155;
-    this.speed = Math.random()* 1.5 + 0.3;
+    this.speed = Math.random()* 1.5 + 0.2;
     this.free = true;
     this.angle = 0;
     this.angularVelocity = Math.random() * 0.03 - 0.01;
@@ -26,7 +26,7 @@ export class Asteroid {
       context.save();
       context.translate(this.x, this.y);
       context.rotate(this.angle);
-      context.drawImage(this.image, -this.width * 0.5, -this.height * 0.5, this.width, this.height);
+      context.drawImage(this.image, -this.width * .5, -this.height * 0.5, this.width, this.height);
       context.restore(); 
     }
   }
