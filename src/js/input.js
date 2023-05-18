22743460e6a002c107973427010dd9b6c77d7521
Bai Jaitrong <bjaitrong@comcast.net>
2023-05-18 00:59:13 -0600

    update code to save keyboard event as e.key + 'Hold' if e.repeat = true

--- src/js/input.js
+++ src/js/input.js
@@ -8,18 +8,25 @@ export class InputHandler {
             e.key === 'ArrowUp' ||
             e.key === 'ArrowLeft' ||
             e.key === 'ArrowRight' ||
-            e.key === 'Enter'
-          ) && this.keys.indexOf(e.key) === -1){
+            e.key === 'Enter' ||
+            e.key === 's'
+      ) && this.keys.indexOf(e.key) === -1){
         this.keys.push(e.key);
       } else if (e.key === 'd') this.game.debug = !this.game.debug;
+
+      if (e.repeat && this.keys.indexOf(e.key + 'Hold') === -1){
+          this.keys.push(e.key + 'Hold');
+      }
     });
     window.addEventListener('keyup', e => {
       if ( e.key === 'ArrowDown' ||
-           e.key === 'ArrowUp' ||
-           e.key === 'ArrowLeft' ||
-           e.key === 'ArrowRight' ||
-           e.key === 'Enter'){
+        e.key === 'ArrowUp' ||
+        e.key === 'ArrowLeft' ||
+        e.key === 'ArrowRight' ||
+        e.key === 'Enter' ||
+        e.key === 's'){
         this.keys.splice(this.keys.indexOf(e.key), 1);
+        this.keys.splice(this.keys.indexOf(e.key + 'Hold'),1);
       }
     });
   }
