01658d679ebb4e973b4a7d24d8a762f84ad8d36c
Bai Jaitrong <bjaitrong@comcast.net>
2023-06-02 18:12:14 -0600

    remove projectile class from file

--- src/js/particles.js
+++ src/js/particles.js
@@ -32,7 +32,7 @@ export class ForwardThrusterParticle extends Particle {
     }
   }
   draw(context){
-    if(!this.free){
+    if (!this.free){
       context.save();
       context.translate(this.x, this.y);
       context.rotate(this.angle);
@@ -80,41 +80,5 @@ export class ReverseThrusterParticle extends Particle {
     this.free = false;
     this.x = this.game.player.x + this.game.player.width * 0.8 ;
     this.y = this.game.player.y + this.game.player.height * 0.8; 
-  } 
-}
-export class Projectiles extends Particle {
-  constructor(game, x, y){
-    super(game);
-    this.image = document.getElementById('projectileFrame5');
-    this.size = Math.random() * 80 + 3;
-    this.x = x;
-    this.y = y - 8;
-    this.dx = 10;
-    this.dy = 0;
-    this.angle = 0;
-    this.angularVelocity = Math.random() * 0.2 - 0.1;
-  }
-  update(deltaTime){
-    if(!this.free){
-      this.x += this.dx * deltaTime + this.game.speed;
-      this.y += this.dy;
-      this.angle += this.angularVelocity;
-      this.x += Math.sin(this.angle * 10);
-      if (this.x > this.game.width) this.reset();
-    }
-  }
-  draw(context){
-    if(!this.free){
-      context.save();
-      context.translate(this.x, this.y);
-      context.rotate(this.angle);
-      context.drawImage(this.image, -this.size * 0.5, -this.size * 0.5, this.size, this.size);
-      context.restore();
-    }
-  }
-  start(){
-    this.free = false;
-    this.x = this.game.player.x + this.game.player.width * .16 ;
-    this.y = this.game.player.y + this.game.player.height * 0.5; 
   } 
 }
